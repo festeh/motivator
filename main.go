96d060928ec52890083895e06d1fe9523556c089
Dima Lipin <dimich3d@ya.ru>
2024-06-16 15:36:20 +0200

    more strict check

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 type Status struct {
@@ -34,7 +36,7 @@ type ChromePref struct {
 	} `json:"extensions"`
 }
 
-func queryDmn() (bool, error) {
+func queryExtensionEnabled() (bool, error) {
 	// Get Google chrome preferences
 	// Check if Distact Me is enabled
 	// Set status.DmnOn to true or false
@@ -64,12 +66,41 @@ func queryDmn() (bool, error) {
 	return false, fmt.Errorf("Distact Me Not extension not found")
 }
 
-func main() {
-	var status = Status{DmnOn: false}
-	status.DmnOn, status.Error = queryDmn()
+func queryStatus() (bool, error) {
+	status := true
+	chromeExtPath := filepath.Join("/home", os.Getenv("USER"), ".config", "google-chrome", "Default", "Local Extension Settings", "lkmfokajfoplgdkdifijpffkjeejainc", "000003.log")
+	data, err := os.ReadFile(chromeExtPath)
+	if err != nil {
+		return false, err
+	}
+	lines := strings.Split(string(data), "\n")
+	lastLine := lines[len(lines)-1]
+	enabledIndex := strings.LastIndex(lastLine, "Enabled")
+	if enabledIndex == -1 {
+		return status, nil
+	}
+	enabledValue := strings.TrimSpace(lastLine[enabledIndex+8:])
+	if strings.ToLower(enabledValue) == "false" {
+		status = false
+	}
+	return status, nil
+}
+
+func writeStatus(status Status) {
 	str, err := json.Marshal(status)
 	if err != nil {
 		fmt.Println(err)
 	}
 	os.Stdout.Write(str)
+}
+
+func main() {
+	var status = Status{DmnOn: false}
+	status.DmnOn, status.Error = queryExtensionEnabled()
+	if status.DmnOn == false {
+		writeStatus(status)
+		os.Exit(0)
+	}
+	status.DmnOn, status.Error = queryStatus()
+	writeStatus(status)
 }
